02d3acea0d247d660ff16f22cb9ca19713fee3e8
Rydzanicz <asddsfsdfsf@gmail.com>
2025-02-08 10:46:31 +0100

    Updated styles for consistency and improved layouts; enhanced content for About and Privacy pages

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -2,11 +2,10 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {FooterComponent} from './footer/footer.component';
 import {HeaderComponent} from './header/header.component';
-import {ShopComponent} from './shop/shop.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, ShopComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
