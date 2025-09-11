23cfc72f531dee456c1ac971998f62c3d325c64b
Rydzanicz <asddsfsdfsf@gmail.com>
2025-09-11 20:02:31 +0200

    Changed app to test version1

---
+++ my-angular-app/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist/my-angular-app')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/my-angular-app', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
